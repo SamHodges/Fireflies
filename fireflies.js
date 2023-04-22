a03bac86484d708bb4405853ff512194c17ea74a
Sam Hodges <shodges2019@francisparker.org>
2023-04-22 10:56:10 -0400

    started synch circle stoof

---
+++ fireflies.js
@@ -0,0 +1,17 @@
+const SVG_NS = "http://www.w3.org/2000/svg";
+const svg = document.querySelector("#firefly-visual");
+
+function synchronizationCircle(synchronizationDuration, synchronizationRadius){
+	this.radius = synchronizationRadius;
+	this.duration = synchronizationDuration;
+
+	this.initialize = function(){
+		//make a circle with thing at 0
+		// set interval call for update
+	}
+
+	this.update = function(){
+
+	}
+
+}
